82c451ba6ba61f81df8c09a448362a270db61428
shinebyul <qufdl8382@gmail.com>
2024-11-12 18:17:08 +0900

    [PGS] L2 - 두 원 사이의 정수 쌍

--- simulation/level2/PGS_두_원_사이의_정수_쌍.java
+++ simulation/level2/PGS_두_원_사이의_정수_쌍.java
@@ -6,31 +6,15 @@ class PGS_두_원_사이의_정수_쌍 {
         long count = 0;
 
         for(int i=1;i<r2;i++){
-            // System.out.println("i : "+i);
-            int max = (int)Math.sqrt(r2*r2-i*i);
+            int max = (int) Math.sqrt((long) r2 * r2 - (long) i * i);
             int min = 1;
 
             if(i < r1){
-                double dou = Math.sqrt(r1*r1-i*i);
-                if(dou == (double)((int)dou)){
-                    min = (int)dou;
-                }else{
-                    min = (int)(dou+1.0);
-                }
-            }else{
-                min = 1;
+                double dou = Math.sqrt((long) r1 * r1 - (long) i * i);
+                min = dou == (int)dou ? (int)dou : (int)(dou + 1.0);
             }
-            // System.out.println("max : "+max);
 
-            for(int j=min;j<=max;j++){
-                // System.out.println("j : "+j);
-                double r = Math.sqrt(i*i+j*j);
-                // System.out.println("r : "+r);
-                if((double)r1<=r && r<=(double)r2){
-                    count++;
-                    // System.out.println("count : "+ count);
-                }
-            }
+            count+=(max-min+1);
         }
 
         answer = count*4 + 4*(r2-r1+1);
