21231ef207cc1434a09f491a128cd4b198cd0784
706com <ehdgusjdbswl@gmail.com>
2024-11-28 08:54:01 +0900

    [PGS] L2 - 무인도 여행

---
+++ dfs_bfs/level2/PGS_무인도_여행.java
@@ -0,0 +1,86 @@
+//소요시간
+//[241128] : 13분
+
+import java.util.*;
+
+public class 무인도_여행 {
+    static int N,M;
+
+    static char[][] arr;
+    static boolean[][] visited;
+
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    public int[] solution(String[] maps) {
+
+        N = maps.length;
+        M = maps[0].length();
+
+        arr = new char[N][M];
+        visited = new boolean[N][M];
+        int[] answer;
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0 ; i<N; i++){
+            String str = maps[i];
+            for(int j=0; j<M; j++){
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        // System.out.println(Arrays.deepToString(arr));
+
+        for(int i=0 ; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(!visited[i][j] && arr[i][j] != 'X'){
+                    list.add(bfs(i,j));
+                }
+            }
+        }
+        Collections.sort(list);
+        answer = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        if(list.size() == 0){
+            return new int[]{-1};
+        }
+        return answer;
+    }
+    int bfs(int x, int y){
+        int days = arr[x][y]-'0';
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x,y));
+        visited[x][y] = true;
+
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            for(int i=0; i<4; i++){
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+
+                if(nx<0 || ny<0 || nx>=N || ny>=M){
+                    continue;
+                }
+                if(arr[nx][ny] =='X' || visited[nx][ny]){
+                    continue;
+                }
+                q.offer(new Node(nx,ny));
+                visited[nx][ny] = true;
+                days += arr[nx][ny]-'0';
+            }
+        }
+        return days;
+    }
+    class Node{
+        int x;
+        int y;
+        Node(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+}
