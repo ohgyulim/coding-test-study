a47e0c5db9f862cc5082d91f9f10a260dc834a6e
sue06004 <sue06004@naver.com>
2024-11-28 17:27:25 +0900

    [BOJ] L2 - 최대 힙

---
+++ priority_queue/level2/BOJ_최대 힙.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+def solv():
+    N = int(input())
+    heap = []
+    for _ in range(N):
+        x = int(input())
+        if x != 0:
+            heapq.heappush(heap, -x)
+        else:
+            if not heap:
+                print(0)
+            else:
+                print(-heapq.heappop(heap))
+
+solv()
