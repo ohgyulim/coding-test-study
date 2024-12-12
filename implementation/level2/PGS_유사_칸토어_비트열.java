cea866b310d72686b63e005444c44dc298419ed3
syy0O <songie05@naver.com>
2024-12-13 07:17:39 +0900

    [PGS] L2 - 유사 칸토어 비트열

---
+++ implementation/level2/PGS_유사_칸토어_비트열.java
@@ -0,0 +1,13 @@
+package implementation.level2;
+
+class PGS_유사_칸토어_비트열 {
+    //1->5->25->125 ... 로 자리수 늘어남
+    //5^20 = 9.5367432e+13
+    // 분할 정복...?
+    // 시간 더 필요할 듯
+    public int solution(int n, long l, long r) {
+        int answer = 0;
+
+        return answer;
+    }
+}
