aff915b3d5dcd34b02b622ea11aef42372d2fa5e
shinebyul <qufdl8382@gmail.com>
2024-12-16 05:55:40 +0900

    [BJ] G3 - 과제

--- priority-queue/BJ_과제.java
+++ priority-queue/BJ_과제.java
@@ -1,57 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BJ_과제 {
     static class Homework {
-        int d;
-        int w;
+        int deadline;
+        int score;
 
-        Homework(int d, int w) {
-            this.d = d;
-            this.w = w;
+        Homework(int deadline, int score) {
+            this.deadline = deadline;
+            this.score = score;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         List<Homework> homeworks = new ArrayList<>();
-        int maxDay = 0;
-
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int d = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int d = Integer.parseInt(input[0]);
+            int w = Integer.parseInt(input[1]);
             homeworks.add(new Homework(d, w));
-            maxDay = Math.max(maxDay, d);
         }
 
-        int result = 0;
+        homeworks.sort(Comparator.comparingInt(hw -> hw.deadline));
 
-        for (int i = maxDay; i >= 1; i--) {
-            int max = 0;
-            int index = -1;
-
-            for (int j = 0; j < homeworks.size(); j++) {
-                Homework homework = homeworks.get(j);
-
-                if (i <= homework.d && homework.w > max) {
-                    max = homework.w;
-                    index = j;
-                }
-            }
-
-            if (index != -1) {
-                result += max;
-                homeworks.remove(index);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (Homework hw : homeworks) {
+            minHeap.add(hw.score);
+            // 힙 크기가 마감일을 초과하면, 최소 점수를 제거
+            if (minHeap.size() > hw.deadline) {
+                minHeap.poll();
             }
         }
 
-        System.out.println(result);
+        // 힙에 남아 있는 점수 더하기
+        int maxScore = 0;
+        while (!minHeap.isEmpty()) {
+            maxScore += minHeap.poll();
+        }
+
+        System.out.println(maxScore);
     }
 }
