4ee2811e9795563a19348f0ccc9aecae834cc479
shinebyul <qufdl8382@gmail.com>
2024-12-18 11:51:28 +0900

    [PGS] L2 - 귤 고르기

---
+++ simulation/level2/PGS_귤_고르기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class PGS_귤_고르기 {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<tangerine.length;i++){
+            if(map.containsKey(tangerine[i])){
+                map.put(tangerine[i], map.get(tangerine[i])+1);
+            }else{
+                map.put(tangerine[i], 1);
+            }
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (Integer key : map.keySet()) {
+            pq.add(map.get(key));
+        }
+
+        while(!pq.isEmpty()){
+            int p = pq.poll();
+            if(k <= p){
+                answer++;
+                break;
+            }
+            k-=p;
+            answer++;
+        }
+
+        return answer;
+    }
+}
