9e8bdd0c04ed33e7ec63ac2992c4065e106a645c
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-12-20 07:55:57 +0900

    [PGS] L2 - 점 찍기

---
+++ math/level2/PGS_점_찍기.java
@@ -0,0 +1,14 @@
+package math.level2;
+
+public class PGS_점_찍기 {
+	public long solution(int k, int d) {
+		long answer = 0;
+
+		for (long x = 0; x <= d; x += k) {
+			long y = (long) Math.sqrt((long)d * d - x * x);
+			answer += y/k + 1;
+		}
+
+		return answer;
+	}
+}
