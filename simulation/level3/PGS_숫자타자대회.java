9b7689a264dd87aa2088de5b06972d7c423d08d0
sue06004 <sue06004@naver.com>
2024-12-22 22:43:32 +0900

    [PGS] L3 - 숫자 타자 대회

--- simulation/level3/PGS_숫자타자대회.java
+++ simulation/level3/PGS_숫자타자대회.java
@@ -1,123 +1,66 @@
 package simulation.level3;
 
+import java.util.Arrays;
+
+// 해설 봄
+
 public class PGS_숫자타자대회 {
+    int[][][] dp;
+    int len;
+    String nums;
+
+    public int[][] cost = {
+            {1, 7, 6, 7, 5, 4, 5, 3, 2, 3},
+            {7, 1, 2, 4, 2, 3, 5, 4, 5, 6},
+            {6, 2, 1, 2, 3, 2, 3, 5, 4, 5},
+            {7, 4, 2, 1, 5, 3, 2, 6, 5, 4},
+            {5, 2, 3, 5, 1, 2, 4, 2, 3, 5},
+            {4, 3, 2, 3, 2, 1, 2, 3, 2, 3},
+            {5, 5, 3, 2, 4, 2, 1, 5, 3, 2},
+            {3, 4, 5, 6, 2, 3, 5, 1, 2, 4},
+            {2, 5, 4, 5, 3, 2, 3, 2, 1, 2},
+            {3, 6, 5, 4, 5, 3, 2, 4, 2, 1}
+    };
+
     public int solution(String numbers) {
         int answer = 0;
+        //최소한의 시간으로 타이핑을 하는 경우의 가중치 합을 return
 
-        int[][] position = new int[10][2]; // position[i][0]: i의 r좌표, position[i][1]: i의 c좌표
-        for (int i = 0; i < 10; i++) {
-            if (i == 0) {
-                position[i][0] = 3;
-                position[i][1] = 1;
-                continue;
-            }
-            position[i][0] = (i - 1) / 3;
-            position[i][1] = (i - 1) % 3;
+
+        len = numbers.length();
+        nums = numbers;
+        dp = new int[len][10][10]; //idx, left, right
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < 10; j++)
+                Arrays.fill(dp[i][j], -1);
         }
 
-        int lr = 1; // 왼손 엄지 좌표
-        int lc = 0;
+        return solve(0, 4, 6);
 
-        int rr = 1; // 오른손 엄지 좌표
-        int rc = 2;
-
-        int i = 0;
-        while (i < numbers.length()) {
-            int num = numbers.charAt(i) - '0';
-            int targetR = position[num][0];
-            int targetC = position[num][1];
-
-            int leftPoint = calcPoint(lr, lc, targetR, targetC);
-
-            int rightPoint = calcPoint(rr, rc, targetR, targetC);
-
-            if (leftPoint < rightPoint) {
-                lr = targetR;
-                lc = targetC;
-                answer += leftPoint;
-                i++;
-                //System.out.println("left");
-            } else if (leftPoint > rightPoint) {
-                rr = targetR;
-                rc = targetC;
-                answer += rightPoint;
-                i++;
-                //System.out.println("right");
-            } else {
-                int j = i + 1;
-                int curR = targetR;
-                int curC = targetC;
-                int tmpPoint = leftPoint;
-                while (true) {
-                    if (j == numbers.length()) {
-                        answer += tmpPoint;
-                        i = j;
-                        break;
-                    }
-                    int nextNum = numbers.charAt(j) - '0';
-                    int nextTargetR = position[nextNum][0];
-                    int nextTargetC = position[nextNum][1];
-
-                    int nextLeftPoint = calcPoint(lr, lc, nextTargetR, nextTargetC);
-                    int nextRightPoint = calcPoint(rr, rc, nextTargetR, nextTargetC);
-                    int nextCurPoint = calcPoint(curR, curC, nextTargetR, nextTargetC);
-
-                    if ((nextCurPoint <= nextLeftPoint && nextCurPoint <= nextRightPoint) ||
-                            (nextLeftPoint == nextRightPoint)) {
-                        j++;
-                        curR = nextTargetR;
-                        curC = nextTargetC;
-                        if (j == numbers.length()) {
-                            tmpPoint += Math.min(nextCurPoint, nextLeftPoint);
-                        } else {
-                            tmpPoint += nextCurPoint;
-                        }
-                    } else if (nextLeftPoint <= nextCurPoint && nextLeftPoint <= nextRightPoint) {
-                        rr = curR;
-                        rc = curC;
-                        answer += tmpPoint;
-                        //System.out.println("right");
-                        i = j;
-                        break;
-                    } else if (nextRightPoint <= nextCurPoint && nextRightPoint <= nextLeftPoint) {
-                        lr = curR;
-                        lc = curC;
-                        answer += tmpPoint;
-                        //System.out.println("left");
-                        i = j;
-                        break;
-                    }
-                }
-            }
-
-        }
-
-        return answer;
     }
 
-    private int calcPoint(int r, int c, int targetR, int targetC) {
-        int point = 0;
-
-        int distanceR = Math.abs(targetR - r);
-        int distanceC = Math.abs(targetC - c);
-        if (distanceR == 0 && distanceC == 0) {
-            point = 1;
-        } else {
-            while (distanceR > 0 || distanceC > 0) {
-                if (distanceR > 0 && distanceC > 0) {
-                    point += 3;
-                    distanceR--;
-                    distanceC--;
-                } else if (distanceR > 0) {
-                    point += 2;
-                    distanceR--;
-                } else if (distanceC > 0) {
-                    point += 2;
-                    distanceC--;
-                }
-            }
+    public int solve(int idx, int left, int right) {
+        if (idx == len) {
+            return 0; //문자열 끝까지 탐색한 경우
         }
-        return point;
+        //이미 계산된 경우
+        if (dp[idx][left][right] != -1) return dp[idx][left][right];
+
+        int num = nums.charAt(idx) - '0';
+        int ans = Integer.MAX_VALUE;
+
+        //왼쪽 손가락을 움직이는 경우
+        if (num != right) {
+            ans = Math.min(solve(idx + 1, num, right) + cost[left][num], ans);
+        }
+
+        //오른쪽 손가락을 움직이는 경우
+        if (num != left) {
+            ans = Math.min(solve(idx + 1, left, num) + cost[right][num], ans);
+        }
+
+        return dp[idx][left][right] = ans;
     }
 }
 
