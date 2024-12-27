934f86e2b20f41863a69cc98ec314b40ea4f6ced
706com <ehdgusjdbswl@gmail.com>
2024-12-27 20:24:53 +0900

    [PGS] L3 - 등대

--- dfs_bfs/level3/PGS_등대.java
+++ dfs_bfs/level3/PGS_등대.java
@@ -1,16 +1,12 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] lights;
-    static boolean[] visited;
     static List<Integer>[] list;
+    static int[] haveToTurnOn;
 
     public int solution(int n, int[][] edges) {
-        lights = new boolean[n+1];
-        visited = new boolean[n+1];
         list = new ArrayList[n+1];
-
-        visited[1] = true;
+        haveToTurnOn = new int[n+1];
 
         // 인접리스트 입력
         for (int i = 1; i <= n; i++) {
@@ -21,39 +17,42 @@ class Solution {
             list[edge[1]].add(edge[0]);
         }
 
-        turnLight(1, 1);
+        dfs(1, 0);
+        // System.out.println(Arrays.toString(haveToTurnOn));
 
-        int count = 0;
-        for (boolean lightOn: lights) {
-            if (lightOn) count++;
+        int result = 0;
+        for(int i=1; i<=n; i++){
+            if(haveToTurnOn[i] != 0){
+                result++;
+            }
         }
 
-        return count;
+        return result;
     }
 
     // dfs
-    public void turnLight(int curr, int parent) {
-        for (int x: list[curr]) {
-            if (!visited[x]) {
-                visited[x] = true;
-                turnLight(x, curr);
-            }
-        }
+    // 1과 0을 반대로 생성한 이유 -> return 값이 이래야 부모노드
+    public int dfs(int cur, int parent) {
+        // 리프노드면 1 반환 (킬 필요가 있는 상태)
+        if (list[cur].size() == 1 && list[cur].get(0) == parent)
+            return 1;
 
-        // 부모 제외 자식 노드 체크
-        boolean hasChild = false;
-        for (int x: list[curr]) {
-            if (x == parent) {
+        // 리프노드가 아니면 dfs 탐색
+        for (int i=0; i < list[cur].size(); i++) {
+            int next = list[cur].get(i);
+            //다음이 부모면 생략
+            if (next == parent) {
                 continue;
             };
-            if (!lights[x]) {
-                hasChild = true;
-                break;
-            }
+            // return 되는 값 저장 (자식들의 리턴값)
+            haveToTurnOn[cur] += dfs(next, cur);
         }
-
-        if (hasChild) {
-            lights[curr] = true;
+        // 자식들이 모두 0 이면 (모두 킬 필요가 없는 상태)
+        // 1 리턴 (나는 킬 필요가 있다 상태)
+        if (haveToTurnOn[cur] == 0){
+            return 1;
         }
+        // 킬 필요가 없음을 리턴
+        return 0;
     }
 }
