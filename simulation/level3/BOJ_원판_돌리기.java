2c8ac3dc4f6b4925689fb33f69385ee40969bbf2
706com <ehdgusjdbswl@gmail.com>
2024-12-27 20:34:40 +0900

    [BOJ] L3 - 원판 돌리기 (❌주말)

---
+++ simulation/level3/BOJ_원판_돌리기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class _17822_원판_돌리기 {
+    static int N,M,T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+    }
+}
