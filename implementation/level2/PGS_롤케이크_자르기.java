00936b9b453f882cd4f3d57abb1169c2faab0c3b
syy0O <songie05@naver.com>
2024-12-30 14:20:03 +0900

    [PGS] L2 - 롤케이크 자르기

---
+++ implementation/level2/PGS_롤케이크_자르기.java
@@ -0,0 +1,27 @@
+package implementation.level2;
+
+import java.util.*;
+
+class PGS_롤케이크_자르기 {
+    public int solution(int[] topping) {
+        int answer = 0;
+
+        int[] types = new int[topping.length];
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<topping.length;i++) {
+            set.add(topping[i]);
+            types[i] = set.size();
+        }
+
+        Set<Integer> temp = new HashSet<>();
+        for (int i=topping.length-1;i>0;i--) {
+            temp.add(topping[i]);
+            if(temp.size() == types[i-1]) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
+
