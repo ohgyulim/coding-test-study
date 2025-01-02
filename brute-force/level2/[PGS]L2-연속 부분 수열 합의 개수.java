d461b191e42ab20e3363f620db53414ed9060046
dlflq11@gmail.com <dlflq11@gmail.com>
2025-01-02 18:02:57 +0900

    [PGS]L2 - 연속 부분 수열 합의 개수

---
+++ brute-force/level2/[PGS]L2-연속 부분 수열 합의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        int n = elements.length;
+        for(int i = 0; i <= n; i++){
+            int sum = 0;
+            for(int j = 0; j < n; j++){
+                sum += elements[(i+j)%n];
+                set.add(sum);
+            }
+        }
+        return set.size();
+    }
+}
