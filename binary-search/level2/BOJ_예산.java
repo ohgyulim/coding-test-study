663f6f18ea9beba0ffd36aae4b2b834355106291
706com <ehdgusjdbswl@gmail.com>
2025-01-10 15:10:54 +0900

    [BOJ] L2 - 예산

--- binary-search/level2/BOJ_예산.java
+++ binary-search/level2/BOJ_예산.java
@@ -2,31 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class _2512_예산 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int left=0;
-        int right= 0;
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
+
+        int left = 0;
+        int right = 0;
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            right = Math.max(right, arr[i]);
+            right = Math.max(right,arr[i]);
         }
 
-        int m = Integer.parseInt(br.readLine());
-        while(left<=right) {
-            int sum = (left+right)/2;
-            long budget =0;
-            for(int i=0; i<n; i++) {
-                if(arr[i]>mid) sum += mid;
-                else sum+= arr[i];
+        int maximum = Integer.parseInt(br.readLine());
+
+        while(left<=right){
+            int mid = (left + right) / 2;
+            long sum = 0;
+
+            for(int i=0; i<N; i++){
+                sum += Math.min(mid,arr[i]);
             }
-            if(sum<=m) {
-                left = mid+1;
-            }else {
-                right = mid-1;
+
+            if(sum<=maximum){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
             }
         }
         System.out.println(right);
