410dc21dd7c94ae1c7f99142d04ff0f64df24f9a
sue06004 <sue06004@naver.com>
2025-01-12 13:46:47 +0900

    [BOJ] L2 - 레벨 햄버거

--- simulation/level2/L2_레벨 햄버거.py
+++ simulation/level2/L2_레벨 햄버거.py
@@ -2,9 +2,32 @@ import sys
 
 input = sys.stdin.readline
 
-def solv():
-    N, M = map(int,input().split())
+N, X = map(int,input().split())
+
+burger = [1] * 51            # 버거의 레이아웃 갯수
+patty = [1] * 51             # 버거의 패티 갯수
+
+for i in range(1, N+1):
+    burger[i] = 1 + burger[i-1] + 1 + burger[i-1] + 1
+    patty[i] = patty[i-1] + 1 + patty[i-1]
 
 
+def eat(n, x):                      # 먹은 패티 갯수
+    if n == 0:
+        return x
+    if x == 1:
+        return 0
+    elif x <= 1 + burger[n-1]:						# case 1
+        return eat(n-1, x-1)                # 맨 아래 번 빼고 (x-1)
+    elif x == 1 + burger[n-1] + 1:                  # case 2 딱 ~가운데 패티까지 먹은 경우
+        return patty[n-1] + 1
+    elif x <= burger[n-1] + burger[n-1] + 1 + 1:	# case 3
+        return patty[n-1] + 1 + eat(n-1, (x-(burger[n-1]+2)))
+    else:											# case 4
+        return patty[n]
 
-solv()
+print(eat(N, X))
+
+# 해설 참고
+# https://velog.io/@hyebinnn/%EB%B0%B1%EC%A4%80-%EB%A0%88%EB%B2%A8-%ED%96%84%EB%B2%84%EA%B1%B0-16974%EB%B2%88
+
