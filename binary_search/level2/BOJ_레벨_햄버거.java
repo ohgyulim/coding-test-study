bda96226d6fd0344ed7dee8e9047f1ba384cafdc
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-01-13 13:06:55 +0900

    [BOJ] - L2 레벨 햄버거

--- binary_search/level2/BOJ_레벨_햄버거.java
+++ binary_search/level2/BOJ_레벨_햄버거.java
@@ -5,25 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 1. level N 부터 level 1 영역까지 쪼갠다
+// 2. level 1 영역에서의 X 위치를 찾고, level 1 영역의 X 위치에 대한 패티의 개수를 구한다
+// 3. level 1 영역의 패티의 수 부터 return하면서 더한다
 public class BOJ_레벨_햄버거 {
-	// 1. level N 부터 level 1 영역까지 쪼갠다
-	// 2. level 1 영역에서의 X 위치를 찾고, level 1 영역의 X 위치에 대한 패티의 개수를 구한다
-	// 2. (level N - 1 패티의 수) / 2 + 2.에서 구한 값
+	static long[] layers;
+	static long[] patties;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		long X = Long.parseLong(st.nextToken());
-		long[] dp = new long[N + 1];
-		dp[1] = 5;
-		for (int i = 2; i <= N; i++) {
-			dp[i] = (dp[i - 1] * 2) + 3;
+
+		layers = new long[N + 1];
+		patties = new long[N + 1];
+		layers[0] = 1;
+		patties[0] = 1;
+
+		for (int i = 1; i <= N; i++) {
+			layers[i] = layers[i - 1] * 2 + 3;
+			patties[i] = patties[i - 1] * 2 + 1;
 		}
 
-		long left = 1;
-		long right = dp[N];
-		// long patty = (long)(3 * Math.pow(2, N-1));
+		System.out.println(recur(N, X));
+	}
 
-		System.out.println();
+	public static long recur(int level, long x) {
+		if (level == 0) {
+			return 1;
+		}
+
+		long half = layers[level - 1]; // 길이
+
+		if (x == 1) {
+			return 0;
+		} else if (x <= half + 1) { // mid 보다 왼쪽에 있으면
+			return recur(level - 1, x - 1); // 맨 앞 한 칸 뺌
+		} else if (x == half + 2) { // half + (맨 앞 B + 맨 뒤 B)를 하면 mid값임
+			return patties[level - 1] + 1; // mid가 패티이기 때문에 + 1
+		} else { // mid보다 오른쪽에 있으면
+			return patties[level - 1] + 1 + recur(level - 1, x - half - 2);
+		}
 	}
 }
