f5f96e26866198363a17da5a8ed0ef7fc7753e06
706com <ehdgusjdbswl@gmail.com>
2025-01-14 19:13:42 +0900

    [PGS] L3 - 파괴되지 않은 건물

--- prefix-sum/level3/PGS_파괴되지_않은_건물.java
+++ prefix-sum/level3/PGS_파괴되지_않은_건물.java
@@ -1,1 +1,58 @@
-// 실패.
+import java.util.*;
+
+public class 파괴되지_않은_건물 {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        int N = board.length;   //4
+        int M = board[0].length;    //5
+
+        int[][] prefix = new int[N+1][M+1];
+
+        // 1) 누적합 입력
+        for(int i=0; i<skill.length; i++){
+            int r1 = skill[i][1];
+            int c1 = skill[i][2];
+            int r2 = skill[i][3];
+            int c2 = skill[i][4];
+            int damage = skill[i][5];
+
+            //공격
+            if(skill[i][0] == 1){
+                damage = -damage;
+            }
+
+            prefix[r1][c1] += damage;
+            prefix[r1][c2+1] += -damage;
+            prefix[r2+1][c1] += -damage;
+            prefix[r2+1][c2+1] += damage;
+        }
+
+        // 2) 누적합 계산 - 가로
+        for(int i=0; i<=N; i++){
+            for(int j=1; j<=M; j++){
+                prefix[i][j] += prefix[i][j-1];
+            }
+        }
+
+        // 3) 누적합 계산 - 세로
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<=M; j++){
+                prefix[i][j] += prefix[i-1][j];
+            }
+        }
+
+        // for(int[] x : prefix){
+        //     System.out.println(Arrays.toString(x));
+        // }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                board[i][j] += prefix[i][j];
+                if(board[i][j] > 0){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
