c09f19f9551c2f27496acc87a5c118b54b45a984
shinebyul <qufdl8382@gmail.com>
2025-01-18 09:53:58 +0900

    [PGS] L2 - K개수에서 소수 개수 구하기

---
+++ simulation/level2/PGS_K수에서_소수_개수_구하기.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+
+class PGS_K수에서_소수_개수_구하기 {
+
+    public int solution(int n, int k) {
+        String kBaseNumber = Integer.toString(n, k);
+
+        String[] candidates = kBaseNumber.split("0");
+
+        int primeCount = 0;
+        for (String candidate : candidates) {
+            if (!candidate.isEmpty() && isPrime(Long.parseLong(candidate))) {
+                primeCount++;
+            }
+        }
+
+        return primeCount;
+    }
+
+    private boolean isPrime(long num) {
+        if (num < 2) {
+            return false;
+        }
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
