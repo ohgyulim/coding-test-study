b788c8d8c3b74157bd23448eca2fd786be48bfba
706com <ehdgusjdbswl@gmail.com>
2025-01-21 01:29:04 +0900

    [PGS] L2 - 피로도

---
+++ dfs_bfs/level2/PGS_피로도.java
@@ -0,0 +1,21 @@
+class 피로도 {
+    static int max = 0;
+    boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k,0,dungeons);
+        return max;
+    }
+    public void dfs(int fatigue,int count,int[][] dungeons){
+        // System.out.println(fatigue);
+        max = Math.max(max, count);  // 최대 탐험 수 갱신
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && fatigue >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(fatigue - dungeons[i][1], count + 1, dungeons);
+                visited[i] = false;
+            }
+        }
+    }
+}
