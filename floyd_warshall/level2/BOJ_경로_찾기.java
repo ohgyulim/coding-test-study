0be4f8658b2ca5c450a9223ab0e7fe4bc13b1129
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-01-27 18:45:46 +0900

    [BOJ] L2 - 경로 찾기

---
+++ floyd_warshall/level2/BOJ_경로_찾기.java
@@ -0,0 +1,41 @@
+package floyd_warshall.level2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_경로_찾기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int mid = 0; mid < N; mid++) {
+			for (int start = 0; start < N; start++) {
+				for (int end = 0; end < N; end++) {
+					if (map[start][mid] == 1 && map[mid][end] == 1) {
+						map[start][end] = 1;
+					}
+				}
+			}
+		}
+
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				sb.append(map[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.print(sb);
+	}
+}
