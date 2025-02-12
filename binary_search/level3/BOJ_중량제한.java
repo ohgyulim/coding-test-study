f5d4b96d2e081fda11ef713fd2b6f4352c3c2453
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-02-13 03:53:16 +0900

    [BOJ] L3 - 중량제한

--- binary_search/level3/BOJ_중량제한.java
+++ binary_search/level3/BOJ_중량제한.java
@@ -1,74 +1,81 @@
 package binary_search.level3;
 
+import java.io.*;
 import java.util.*;
 
 public class BOJ_중량제한 {
-	static int N, M;
-	static List<List<Edge>> graph = new ArrayList<>();
-	static int start, end;
-
 	static class Edge {
-		int to, weight;
+		int to;
+		int weight;
 		Edge(int to, int weight) {
 			this.to = to;
 			this.weight = weight;
 		}
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+	static int N;
+	static Map<Integer, List<Edge>> graph = new HashMap<>();
+	static int from, to;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
+		N = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			graph.put(i, new ArrayList<>());
 		}
 
 		int maxWeight = 0;
+		int M = Integer.parseInt(st.nextToken());
 		for (int i = 0; i < M; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			int C = sc.nextInt();
-			graph.get(A).add(new Edge(B, C));
-			graph.get(B).add(new Edge(A, C));
-			maxWeight = Math.max(maxWeight, C);
+			st = new StringTokenizer(br.readLine(), " ");
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			graph.get(from).add(new Edge(to, weight));
+			graph.get(to).add(new Edge(from, weight));
+			maxWeight = Math.max(maxWeight, weight);
 		}
 
-		start = sc.nextInt();
-		end = sc.nextInt();
+		st = new StringTokenizer(br.readLine(), " ");
+		from = Integer.parseInt(st.nextToken());
+		to = Integer.parseInt(st.nextToken());
 
 		System.out.println(binarySearch(maxWeight));
+
+
 	}
 
-	static int binarySearch(int maxWeight) {
-		int left = 1, right = maxWeight, answer = 0;
-
+	public static int binarySearch(int weight) {
+		int left = 0;
+		int right = weight;
+		int maxWeight = 0;
 		while (left <= right) {
 			int mid = (left + right) / 2;
-			if (canCross(mid)) {
-				answer = mid;
+			if (canAnswer(mid)) {
+				maxWeight = mid;
 				left = mid + 1;
 			} else {
 				right = mid - 1;
 			}
 		}
-		return answer;
+		return maxWeight;
 	}
 
-	static boolean canCross(int weightLimit) {
+	public static boolean canAnswer(int weight) {
 		Queue<Integer> queue = new LinkedList<>();
 		boolean[] visited = new boolean[N + 1];
-		queue.add(start);
-		visited[start] = true;
+		queue.offer(from);
+		visited[from] = true;
 
 		while (!queue.isEmpty()) {
-			int cur = queue.poll();
-			if (cur == end) return true;
+			int edge = queue.poll();
+			if (edge == to) return true;
 
-			for (Edge edge : graph.get(cur)) {
-				if (!visited[edge.to] && edge.weight >= weightLimit) {
-					visited[edge.to] = true;
-					queue.add(edge.to);
+			for (Edge target : graph.getOrDefault(edge, new ArrayList<>())) {
+				if (!visited[target.to] && target.weight >= weight) {
+					visited[target.to] = true;
+					queue.offer(target.to);
 				}
 			}
 		}
