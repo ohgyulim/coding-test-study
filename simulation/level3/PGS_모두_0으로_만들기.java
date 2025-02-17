29e3361dc79fcb4570ff4a65fd883391dced5964
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-02-17 15:29:30 +0900

    [PGS] L3 - 모두 0으로 만들기 (해결)

--- simulation/level3/PGS_모두_0으로_만들기.java
+++ simulation/level3/PGS_모두_0으로_만들기.java
@@ -3,67 +3,70 @@ package simulation.level3;
 import java.util.*;
 
 public class PGS_모두_0으로_만들기 {
-	class Edge {
-		int number;
-		int weight;
-		Edge(int number, int weight) {
-			this.number = number;
-			this.weight = weight;
-		}
-	}
-
-	Map<Integer, List<Edge>> graph = new HashMap<>();
-	PriorityQueue<Edge> edges = new PriorityQueue<>((o1, o2) -> Math.abs(o2.weight) - Math.abs(o1.weight));
+	List<Integer>[] graph;
+	int n;
+	long[] weights;
 	public long solution(int[] a, int[][] edges) {
-		long answer = -1;
-		for (int i = 0; i < a.length; i++) {
-			graph.put(i, new ArrayList<>());
-			edges.offer(new Edge(i, a[i]));
+		n = a.length;
+		graph = new ArrayList[n];
+		weights = new long[n];
+
+		long sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += a[i];
+			weights[i] = (long)a[i];
+			graph[i] = new ArrayList<>();
 		}
+
+		if (sum != 0) return -1;
 
 		for (int[] edge : edges) {
 			int from = edge[0];
 			int to = edge[1];
-			graph.get(from).add(new Edge(to, a[to]));
-			graph.get(to).add(new Edge(from, a[from]));
+			graph[from].add(to);
+			graph[to].add(from);
 		}
 
-		return calc();
+		return recur(0);
 	}
 
-	public int calc() {
-		int count = 0;
-		while (!edges.isEmpty()) {
-			Edge edge = edges.poll();
-			int number = edge.number;
-			int weight = edge.weight;
-			if (weight == 0) continue;
-			for (Edge child : graph.get(number)) {
-				if (!edges.remove(child)) continue;
-				if (weight < 0 && child.weight < 0) continue;
-				if (weight > 0 && child.weight > 0) continue;
-				while (weight > 0) {
-					weight -= 1;
-					child.weight += 1;
-					count += 1;
-				}
-				while (weight < 0) {
-					weight += 1;
-					child.weight -= 1;
-					count += 1;
-				}
-				edges.offer(child);
-			}
-		}
-		return count;
-	}
+	public long recur(int root) {
+		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> stack2 = new Stack<>();
+		stack.push(root);
 
-	public void print() {
-		for (var entrySet : graph.entrySet()) {
-			System.out.println("노드: " + entrySet.getKey());
-			for (Edge edge : entrySet.getValue()) {
-				System.out.println(edge.number + " " + edge.weight);
+		boolean[] visited = new boolean[n];
+		visited[root] = true;
+
+		while (!stack.isEmpty()) {
+			int parent = stack.pop();
+
+			List<Integer> nodes = graph[parent];
+			for (int child : nodes) {
+				if (!visited[child]) {
+					visited[child] = true;
+					stack.push(child);
+					stack2.push(child);
+				}
 			}
 		}
+
+		visited = new boolean[n];
+		long w = 0;
+		while(!stack2.isEmpty()) {
+			int child = stack2.pop();
+			long weight = weights[child];
+			List<Integer> nodes = graph[child];
+			visited[child] = true;
+			for (int parent : nodes) {
+				if (!visited[parent]) {
+					weights[parent] += weight;
+				}
+			}
+			w += Math.abs(weight);
+		}
+
+		return w;
+
 	}
 }
