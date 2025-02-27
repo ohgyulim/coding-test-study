3095cb5be7707888b8d89a435b62a14f88c5e7a5
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-02-27 14:21:16 +0900

    [BOJ] L2 - 설탕 배달 (해결)

--- greedy/level2/BOJ_설탕_배달.java
+++ greedy/level2/BOJ_설탕_배달.java
@@ -1,4 +1,22 @@
 package greedy.level2;
 
+import java.io.*;
+
 public class BOJ_설탕_배달 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+		while (N > 0) {
+			if (N % 5 == 0) {
+				answer += (N / 5);
+				N = 0;
+				break;
+			}
+			N -= 3;
+			answer += 1;
+		}
+		if (N < 0) System.out.println(-1);
+		else System.out.println(answer);
+	}
 }
