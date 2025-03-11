4b752516f9911c693ce99f9d0aed0e38dfb6dcc2
706com <ehdgusjdbswl@gmail.com>
2025-03-11 20:53:23 +0900

    [PGS] L2 - 멀쩡한 사각형

---
+++ math/level2/PGS_멀쩡한_사각형.java
@@ -0,0 +1,24 @@
+public class 멀쩡한_사각형 {
+    public long solution(int w, int h) {
+        long answer = 1;
+
+        if(w<h){
+            int temp = w;
+            w = h;
+            h = temp;
+        }
+
+        long gcd = getGCD(w,h);
+        // System.out.println(gcd);
+
+        return (long)w * (long)h - (w + h - gcd);   //규칙
+    }
+
+    public long getGCD(int a, int b){
+        if(a%b == 0){
+            return b;
+        }
+
+        return getGCD(b, a%b);
+    }
+}
