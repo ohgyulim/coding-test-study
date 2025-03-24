f1699a54960d6d1e1465d1dba87ab3a48318777e
706com <ehdgusjdbswl@gmail.com>
2025-03-25 01:45:25 +0900

    [BOJ] L3 - 카드 정렬하기

---
+++ priority-queue/level3/BOJ_카드_정렬하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class _1715_카드_정렬하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        for(int i=0; i<N; i++){
+            pq.offer(Long.parseLong(br.readLine()));
+        }
+
+        long result = 0;
+        while(pq.size()>=2){
+            long num1= pq.poll();
+            long num2 = pq.poll();
+
+            result += (num1+num2);
+            pq.offer(num1+num2);
+        }
+        System.out.println(result);
+    }
+}
