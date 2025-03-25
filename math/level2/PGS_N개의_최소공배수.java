b2db70bdd498ce5bac1f850469c96e28eadc79cb
706com <ehdgusjdbswl@gmail.com>
2025-03-26 02:20:20 +0900

    [PGS] L2 - N개의 최소공배수

---
+++ math/level2/PGS_N개의_최소공배수.java
@@ -0,0 +1,19 @@
+public class N개의_최소공배수 {
+    public int solution(int[] arr) {
+        int answer = 0;
+
+        int num = arr[0];
+        // 이전 값의 최소 공배수를 들고오기
+        for(int i=1; i<arr.length; i++){
+            int gcd = gcd(num,arr[i]);  // 이전 최소공배수 값과 현재 값의 최대공약수
+            num = (num * arr[i]) / gcd;
+        }
+        return num;
+    }
+    public int gcd(int a, int b){
+        if(a%b == 0){
+            return b;
+        }
+        return gcd(b,a%b);
+    }
+}
