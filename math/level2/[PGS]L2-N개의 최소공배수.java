1c83676b899482831ab27c5447d3f4e7638c805e
dlflq11@gmail.com <dlflq11@gmail.com>
2025-05-06 20:57:50 +0900

    [PGS]L2 - N개의 최소공배수

---
+++ math/level2/[PGS]L2-N개의 최소공배수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            lcm = getLCM(lcm,arr[i]);
+        }
+        return lcm;
+    }
+
+    private int getLCM(int a, int b){
+        return a * b / getGCD(a,b);
+    }
+
+    private int getGCD(int a, int b){
+        while(b != 0){
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
