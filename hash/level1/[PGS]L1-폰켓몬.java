de38e93386d86e0c17c0d44551eb6750ac5dedf0
kangkigs <dlflq11@gmail.com>
2025-05-20 19:19:33 +0900

    [PGS]L1 - 폰켓몬

---
+++ hash/level1/[PGS]L1-폰켓몬.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for(int num : nums){
+            set.add(num);
+        }
+        return nums.length/2 <= set.size() ? nums.length/2 : set.size();
+    }
+}
